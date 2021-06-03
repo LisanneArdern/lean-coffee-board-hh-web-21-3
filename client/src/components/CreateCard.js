1cfcaa98b0b12b0923c2b51942d45e9bc2a7d022
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-03 12:35:23 +0200

    Add CreateCard component

--- client/src/components/CreateCard.js
+++ client/src/components/CreateCard.js
@@ -1,6 +1,26 @@
+import styled from 'styled-components/macro'
 
-  import styled from 'styled-components/macro'
-  export default function CreateCard () {
-    return <div>CreateCard</div>
-  }
-  
+export default function CreateCard() {
+  return (
+    <Wrapper>
+      <button>&lt; back</button>
+      <form>
+        <label>
+          Text:
+          <input type="textarea" />
+        </label>
+        <label>
+          Author:
+          <input type="text" />
+        </label>
+
+        <button>Create Card</button>
+      </form>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 20px;
+`
